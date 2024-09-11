c12b77747fc5232fdeb67dc71f50684ce3bedc9a
NicholasOpiyo <nicholas94oduor@gmail.com>
2024-09-11 18:38:15 +0300

    Fixed bug that output '0 month': replaced the 'months_elapsed' condition variable with the variable 'months_spent'

--- clubstints/templatetags/time_elapsed.py
+++ clubstints/templatetags/time_elapsed.py
@@ -40,7 +40,6 @@ def time_at_club(value):
         days_elapsed = f'{days_spent} day'
     
     # no zero
-    # Sep 11 - fixed bug that output '0 month': changed 'months_elapsed' condition variable to 'months_spent'
     if (years_spent != 0 and months_spent != 0 and days_elapsed != 0):  
         time_spent = f'{years_elapsed}, {months_elapsed}, and {days_elapsed}'
 
